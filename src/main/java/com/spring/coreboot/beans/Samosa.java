92e8f0e77182a0d284604ccf53064d1f38f6acf3
sujeetchnp <sujeetchnp@gmail.com>
2025-07-18 15:09:39 -0400

    Bean life cycle using annotation

--- src/main/java/com/spring/coreboot/beans/Samosa.java
+++ src/main/java/com/spring/coreboot/beans/Samosa.java
@@ -1,5 +1,7 @@
 package com.spring.coreboot.beans;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
@@ -8,7 +10,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 @Component
-public class Samosa implements InitializingBean,  DisposableBean {
+public class Samosa {
 
     private LocalDate localDate;
     private Scanner scanner;
@@ -18,8 +20,8 @@ public class Samosa implements InitializingBean,  DisposableBean {
         System.out.println("Samosa bean has been created");
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() throws Exception {
         this.localDate = LocalDate.now();
         System.out.println("date is set to: " +localDate);
         this.scanner=new Scanner(System.in);
@@ -31,12 +33,12 @@ public class Samosa implements InitializingBean,  DisposableBean {
         System.out.println("you are eating samosa");
         System.out.println("Enter your name");
         String name = scanner.next();
-        System.out.println(name + "is eating samosa");
+        System.out.println(name + " is eating samosa");
         System.out.println("=========================================");
 
     }
 
-    @Override
+    @PreDestroy
     public void destroy() throws Exception {
         System.out.println("destroying samosa bean");
         this.localDate=null;
